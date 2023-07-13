6e4b92cb8a580f6b0fe0c7101f789986dfe6064a
Liusy12810 <liusy12810@outlook.com>
2023-07-13 20:17:49 +0800

    removed some junk files

--- test.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let red = "\x1b[31m";
-    let bold = "\x1b[22";
-    let reset = "\x1b[0m";
-    println!("\x1b[31mhello world!\x1b[0m");
-    println!("\x1b[1mhello world!\x1b[0m");
-}
