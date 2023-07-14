5549febe91ca42c776137dad539bfad98e6de1fb
Liusy12810 <liusy12810@outlook.com>
2023-07-14 12:59:42 +0800

    you can fill the functions with todo whenever you need, and that will not show error prompts lol

--- src/trace/surface.rs
+++ src/trace/surface.rs
@@ -16,13 +16,13 @@ pub struct Triangle {}
 impl Surface for Triangle {
     fn intersect(&mut self, ray: &Ray, t0: f64, t1: f64, hits: &mut HitRecord) -> bool {
         let is_hit: bool = false;
-        is_hit
+        todo!("The method has not been implemented!")
     }
 }
 
 pub struct Sphere {}
 impl Surface for Sphere {
     fn intersect(&mut self, ray: &Ray, t0: f64, t1: f64, hits: &mut HitRecord) -> bool {
-        Vec3d(0.0, 0.0, 0.0)
+        todo!("The method has not been implemented!")
     }
 }
