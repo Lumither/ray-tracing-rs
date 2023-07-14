915d04108ea747af2539a83de07cb909baa8f7f8
Liusy12810 <liusy12810@outlook.com>
2023-07-14 09:23:00 +0800

    把sphere简单写了写

--- src/trace/surface.rs
+++ src/trace/surface.rs
@@ -8,13 +8,20 @@ use super::{
 };
 
 pub trait Surface {
-    fn intersect(&mut self, ray: Ray, t0: f64, t1: f64, hits: HitRecord) -> Color;
+    fn intersect(&mut self, ray: &Ray, t0: f64, t1: f64, hits: &mut HitRecord) -> Color;
 }
 
 pub struct Triangle {}
 
 impl Surface for Triangle {
-    fn intersect(&mut self, ray: Ray, t0: f64, t1: f64, hits: HitRecord) -> Color {
+    fn intersect(&mut self, ray: &Ray, t0: f64, t1: f64, hits: &mut HitRecord) -> Color {
+        Vec3d(0.0, 0.0, 0.0)
+    }
+}
+
+pub struct Sphere {}
+impl Surface for Sphere {
+    fn intersect(&mut self, ray: &Ray, t0: f64, t1: f64, hits: &mut HitRecord) -> Color {
         Vec3d(0.0, 0.0, 0.0)
     }
 }
